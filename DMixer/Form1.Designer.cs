c92479bdc8d5bc1667ebd80a0cfff7d7f39678a3
Daven Bigelow <bigelod@mcmaster.ca>
2018-09-08 08:44:54 -0400

    V1
    Initial release

---
+++ DMixer/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DMixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.snooze = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Open DropMix Freestyle mode";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // snooze
+            // 
+            this.snooze.Interval = 250;
+            this.snooze.Tick += new System.EventHandler(this.snooze_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(175, 52);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DMixer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer snooze;
+    }
+}
+
---
+++ DMixer/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DMixer
+{
+    public partial class Form1 : Form
+    {
+        //A simple DropMix randomizer app, apologies for the poor variable naming it was originally just a test project
+        //Originally made by Daven Bigelow 2018, this code is public domain
+        //It will ask you to do one of the following:
+        //[NEW] - Add a card to a slot
+        //[DEL] - Delete any card from a slot
+        //[CLR] - Clear all cards from the board
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!snooze.Enabled)
+            {
+                Random rnd = new Random();
+
+                int result = rnd.Next(1, 2000); //What are our odds?
+                int slot = rnd.Next(1, 6); //Which slot?
+
+                string cmd = "";
+
+                if (result < 1200)
+                {
+                    //Add a card
+                    cmd = "Slots " + slot.ToString() + " [NEW]";
+                }
+                else if (result > 1910 && result < 1950)
+                {
+                    //Clear the board, extremely rare
+                    cmd = "Slot ALL [CLR]";
+                }
+                else if (result >= 1950)
+                {
+                    //Silently skip turn, also rare
+                    cmd = "";
+                }
+                else
+                {
+                    //Remove a card
+                    cmd = "Slot " + slot.ToString() + " [DEL]";
+                }
+
+                if (cmd != "")
+                {
+                    //We got a command!
+                    label1.Text = "Cmd: " + cmd; //Tell the user their command
+
+                    snooze.Enabled = true; //Enable the snooze timer
+
+                    button1.Enabled = true;
+                }
+
+                timer1.Interval = rnd.Next(30000, 90000); //Set our own timer back up
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                //Now we wait for a command
+                timer1.Enabled = true;
+                label1.Text = "Waiting for CMD";
+                button1.Enabled = false;
+            }
+            else if (snooze.Enabled)
+            {
+                //We received a command, now we wait again for it
+                label1.Text = "Waiting for CMD";
+
+                //Reset color back to white
+                Color c = new Color();
+                int r = 255;
+                int g = r;
+                int b = r;
+
+                c = Color.FromArgb(255, r, g, b);
+
+                this.BackColor = c;
+                this.Update();
+
+                snooze.Enabled = false;
+                button1.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Force it to top, set back color
+            this.Focus();
+            this.BringToFront();
+            this.TopLevel = true;
+            this.TopMost = true;
+
+            Color c = new Color();
+            int r = 255;
+            int g = r;
+            int b = r;
+
+            c = Color.FromArgb(255, r, g, b);
+
+            this.BackColor = c;
+            this.Update();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Space)
+            {
+                //Possibly allow pressing button1 this way
+            }
+        }
+
+        private void snooze_Tick(object sender, EventArgs e)
+        {
+            //Remind the user that they can go now
+            Random rnd = new Random();
+
+            Color c = new Color();
+            int r = 255 - rnd.Next(1, 100);
+            int g = r;
+            int b = r;
+
+            c = Color.FromArgb(255, r, g, b);
+
+            this.BackColor = c;
+            this.Update();
+
+            button1.Enabled = true;
+        }
+    }
+}
